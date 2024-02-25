657d2057def6dc0b998a008dea9d1d13323ef310
Paolo Jericho Quiniano <pao2xthepanda@gmail.com>
2024-02-25 16:00:53 +0800

    Added ASCII to Binary Conversion (for AES)

--- main.cpp
+++ main.cpp
@@ -4,21 +4,54 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <bitset>
 using namespace std;
+
+string aesEncAscii(string input, string key, int algorithm){
+    int lengthFactor = 128;
+    while (lengthFactor < (input.length() * 8)){
+        lengthFactor = lengthFactor + 128;
+    }
+    bool inputArray[lengthFactor] = {};
+    int convertDecBin;
+    int outputArray[input.length()] = {};
+    int keyArray[key.length()] = {};
+    for (int i = 0; i < input.length(); ++i){
+        convertDecBin = input[i] - 32;
+        if (convertDecBin >= 0 && convertDecBin <= 94){
+            bitset<8> valueBin = convertDecBin;
+            for(int j = 0, k = 7; j < 8; j++, k--){
+                inputArray[(i * 8) + j] = valueBin[k];
+            }
+        }
+        else {
+            for(int j = 0, k = 7; j < 8; j++, k--){
+                inputArray[(i * 8) + j] = 0;
+            }
+        }
+    }
+    int outputArraySize = sizeof(inputArray) / sizeof(inputArray[0]);
+    string output;
+    for (int i = 0; i < outputArraySize; ++i) {
+        std::string outputLetter = std::to_string(inputArray[i]);
+        output = output + outputLetter;
+        }
+    return output;
+}
 
 string vignereEncAscii(string input, string key, int algorithm){
     int inputArray[input.length()] = {};
     int outputArray[input.length()] = {};
     int keyArray[key.length()] = {};
     for (int i = 0; i < input.length(); ++i){
-        inputArray[i] = input[i] - 64;
+        inputArray[i] = input[i] - 32;
     }
     for (int i = 0; i < key.length(); ++i){
         if (algorithm == 3){
-            keyArray[i] = key[0] - 64;
+            keyArray[i] = key[0] - 32;
         }
         else {
-            keyArray[i] = key[i] - 64;
+            keyArray[i] = key[i] - 32;
         }
     }
     int inputArraySize = sizeof(inputArray) / sizeof(inputArray[0]);
@@ -29,7 +62,7 @@ string vignereEncAscii(string input, string key, int algorithm){
     }
     keyStream.resize(inputArraySize);
     for (int i = 0; i < input.length(); ++i){
-        outputArray[i] = inputArray[i] + keyStream[i] + 64;
+        outputArray[i] = inputArray[i] + keyStream[i] + 32;
         while (outputArray[i] < 32){
             outputArray[i] = outputArray[i] + 95;
         }
@@ -50,14 +83,14 @@ string vignereDecAscii(string input, string key, int algorithm){
     int outputArray[input.length()] = {};
     int keyArray[key.length()] = {};
     for (int i = 0; i < input.length(); ++i){
-        inputArray[i] = input[i] - 64;
+        inputArray[i] = input[i] - 32;
     }
     for (int i = 0; i < key.length(); ++i){
         if (algorithm == 3){
-            keyArray[i] = key[0] - 64;
+            keyArray[i] = key[0] - 32;
         }
         else {
-            keyArray[i] = key[i] - 64;
+            keyArray[i] = key[i] - 32;
         }
     }
     int inputArraySize = sizeof(inputArray) / sizeof(inputArray[0]);
@@ -68,7 +101,7 @@ string vignereDecAscii(string input, string key, int algorithm){
     }
     keyStream.resize(inputArraySize);
     for (int i = 0; i < input.length(); ++i){
-        outputArray[i] = inputArray[i] - keyStream[i] + 64;
+        outputArray[i] = inputArray[i] - keyStream[i] + 32;
         while (outputArray[i] < 32){
             outputArray[i] = outputArray[i] + 95;
         }
@@ -99,10 +132,10 @@ int main(){
         getline(secret, keyInput);
 	    key += keyInput;
     }
-    cout << "1 - Vignere, 2 - Beaufort, 3 - Caesar: ";
+    cout << "1 - Vignere, 2 - Beaufort, 3 - Caesar, 4 - AES: ";
     cin >> algorithm;
-    while (algorithm != 1 && algorithm != 2 && algorithm != 3){
-        cout << "Invalid Value. Enter Valid Input.\n1 - Vignere, 2 - Beaufort, 3 - Caesar: ";
+    while (algorithm != 1 && algorithm != 2 && algorithm != 3 && algorithm != 4){
+        cout << "Invalid Value. Enter Valid Input.\n1 - Vignere, 2 - Beaufort, 3 - Caesar, 4 - AES: ";
         cin >> algorithm;
     }
     cout << "1 - Encryption, 2 - Decryption: ";
@@ -128,6 +161,12 @@ int main(){
     }
     else if (method == 2 && algorithm == 3){
         output = vignereDecAscii(input, key, algorithm);
+    }
+    else if (method == 1 && algorithm == 4){
+        output = aesEncAscii(input, key, algorithm);
+    }
+    else if (method == 2 && algorithm == 4){
+        output = aesEncAscii(input, key, algorithm);
     }
     else {
         return 1;
